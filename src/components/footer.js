99bdbff0245c87f2bf7b2dd7c5390675b9e0d481
Christopher Swinton <chris@theswintons.co.uk>
2022-01-21 12:24:05 +0000

    New 100% Responsive Layout

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Footer = () => (
-  <footer className="footer">
+  <footer>
     <a href="/">Privacy</a>
     <a href="/">Contact</a>
     <span className="poweredby">Powered by Enghouse Networks	</span>
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,60 +5,61 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import * as React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
-import "./icon.css"
-import "./font.css"
-import Sidebar from "./sidebar"
-import Link from "./link"
-import Footer from "./footer"
-
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <Sidebar
-        link1={
-          <Link
-            state="active"
-            linkstate="active-link"
-            icon="key"
-            linkname="Log In"
-            link="/"
-          />
-        }
-        link2={
-          <Link
-            state=""
-            linkstate=""
-            icon="user"
-            linkname="Register"
-            link="404"
-          />
-        }
-      />
-      <main>{children}</main>
-      <Footer />
-    </>
-  )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-export default Layout
+ import * as React from "react"
+ import PropTypes from "prop-types"
+ import { useStaticQuery, graphql } from "gatsby"
+ 
+ import Header from "./header"
+ import "./layout.css"
+ import "./icon.css"
+ import "./font.css"
+ import Sidebar from "./sidebar"
+ import Link from "./link"
+ import Footer from "./footer"
+ 
+ const Layout = ({ children }) => {
+   const data = useStaticQuery(graphql`
+     query SiteTitleQuery {
+       site {
+         siteMetadata {
+           title
+         }
+       }
+     }
+   `)
+ 
+   return (
+     <div className="layout">
+       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+       <Sidebar
+         link1={
+           <Link
+             state="active"
+             linkstate="active-link"
+             icon="key"
+             linkname="Log In"
+             link="/"
+           />
+         }
+         link2={
+           <Link
+             state=""
+             linkstate=""
+             icon="user"
+             linkname="Register"
+             link="404"
+           />
+         }
+       />
+       <main>{children}</main>
+       <Footer />
+     </div>
+   )
+ }
+ 
+ Layout.propTypes = {
+   children: PropTypes.node.isRequired,
+ }
+ 
+ export default Layout
+ 
